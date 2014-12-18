f208c7f8f37fcbdc7d94f9e2fc355f01b20b68c9
rmurray <ryanm@zoovy.com>
2014-12-17 17:23:06 -0800

    sitemap utility fix

--- utilities/nodesitemap/sitemap.js
+++ utilities/nodesitemap/sitemap.js
@@ -58,7 +58,9 @@ if(opts['customurls']){
 // now load all products and categories
 //
 var request = new XMLHttpRequest();
-request.open('GET','http://www.sportsworldchicago.com/jsonapi/call/v201410/appSEOFetch',false);
+request.open('GET','http://'+DOMAIN+'/jsonapi/call/v201410/appSEOFetch',false);
+console.log('this is the...');
+console.log(DOMAIN);
 request.send(null);
 
 var urls = JSON.parse(request.responseText);
