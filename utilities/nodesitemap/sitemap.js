523011b77e008a94c6c25c4711c38e4616b50efc
rmurray <ryanm@zoovy.com>
2015-02-19 18:03:21 -0800

    custom url addition / sitemap no cats
    Added the march madness category to customurls.
    Set the generation algorithm to bypass categories altogether since all
    categories are custom.

--- utilities/nodesitemap/sitemap.js
+++ utilities/nodesitemap/sitemap.js
@@ -77,7 +77,8 @@ for( var i in urls['@OBJECTS'] ) {
 								url = '/product/' + res.id;
 								break;
 						case 'navcat':
-								url = '/category/' + res.id.substr(1) +'/';  // strip leading . in category name
+								//all categories are in customurls.json
+								//url = '/category/' + res.id.substr(1) +'/';  // strip leading . in category name
 								break;
 						case 'list' :
 								// we don't index these.
